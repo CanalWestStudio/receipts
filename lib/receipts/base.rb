f565cddd1da5c9e274585748ea1f46e8a760e36c
matt <matt@matts-MacBook-Pro.local>
2023-12-16 22:36:18 -0500

    remove width from ship details

--- lib/receipts/base.rb
+++ lib/receipts/base.rb
@@ -97,7 +97,7 @@ module Receipts
         ]
       ]
      
-      table(line_items, width: bounds.width, cell_style: {borders: [], inline_format: true, overflow: :expand})
+      table(line_items, cell_style: {borders: [], inline_format: true, padding: [0, 24, 2, 0]})
     end
 
     def render_line_items(line_items, margin_top: 16)
