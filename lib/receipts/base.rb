f006a6887c874362ff70679cf5495999e136d348
matt <matt@matts-MacBook-Pro.local>
2023-12-17 00:37:52 -0500

    col width test

--- lib/receipts/base.rb
+++ lib/receipts/base.rb
@@ -104,7 +104,7 @@ module Receipts
       move_down margin_top
 
       borders = line_items.length - 2
-      table(line_items, width: bounds.width, column_widths: {1 => 120}, cell_style: {border_color: "a3a3a3", inline_format: true, overflow: :expand}) do
+      table(line_items, width: bounds.width, column_widths: {24 => 120}, cell_style: {border_color: "e5e5e5", inline_format: true, overflow: :expand}) do
         cells.padding = 6
         cells.borders = []
         row(0..borders).borders = [:bottom]
@@ -125,7 +125,7 @@ module Receipts
       stroke do
         line_width 1
         move_down 30
-        stroke_color '737373'
+        stroke_color 'd4d4d4'
         stroke_horizontal_rule
       end
 
