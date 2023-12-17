8fe93f61b22d45391af402d6badc4ae17aef2a77
matt <matt@matts-MacBook-Pro.local>
2023-12-16 20:39:42 -0500

    add CustomsDocument

--- lib/receipts.rb
+++ lib/receipts.rb
@@ -5,6 +5,7 @@ require "prawn/table"
 
 module Receipts
   autoload :Base, "receipts/base"
+  autoload :CustomsDocument, "receipts/customs_document"
   autoload :Invoice, "receipts/invoice"
   autoload :Receipt, "receipts/receipt"
   autoload :PackingSlip, "receipts/packing_slip"
---
+++ lib/receipts/customs_document.rb
@@ -0,0 +1,6 @@
+module Receipts
+  class CustomsDocument < Base
+    @title = "Commercial Invoice"
+    @company = nil
+  end
+end
