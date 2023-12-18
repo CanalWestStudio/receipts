d7ad294fd474bdca1a4e0b2038dfc4f8baf6ac6f
matt <matt@matts-MacBook-Pro.local>
2023-12-17 21:52:35 -0500

    render_shipping_details

--- lib/receipts/base.rb
+++ lib/receipts/base.rb
@@ -24,9 +24,11 @@ module Receipts
       company ||= attributes.fetch(:company)
       render_company(company: company) if company.present?
 
+      recipients ||= attributes.fetch(:recipients)
+      render_shipping_details(recipients: recipients) if recipients.any?
+
       header
       render_details attributes.fetch(:details)
-      render_shipping_details attributes.fetch(:recipients)
       render_line_items attributes.fetch(:line_items)
       render_footer attributes.fetch(:footer)
     end
@@ -89,7 +91,7 @@ module Receipts
       table(line_items, width: bounds.width, cell_style: {borders: [], inline_format: true, overflow: :expand})
     end
 
-    def render_shipping_details(recipients, margin_top: 16)
+    def render_shipping_details(recipients:, margin_top: 16)
       move_down margin_top
 
       recipients.each do |recipient|
