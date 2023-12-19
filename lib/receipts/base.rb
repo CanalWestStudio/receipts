f77a09e4b47c244ad8b10918e66bcb3bff99eb57
matt <matt@matts-MacBook-Pro.local>
2023-12-18 21:15:35 -0500

    fixed width col0

--- lib/receipts/base.rb
+++ lib/receipts/base.rb
@@ -104,10 +104,11 @@ module Receipts
       move_down margin_top
 
       borders = line_items.length - 2
-      table(line_items, width: bounds.width, cell_style: {border_color: "eeeeee", inline_format: true, min_width: 80, max_width: 200}) do
+      table(line_items, width: bounds.width, cell_style: {border_color: "eeeeee", inline_format: true}) do
         cells.padding = 6
         cells.borders = []
 
+        column(0).width = 240
         column(-1).style align: :right
 
         row(0..borders).borders = [:bottom]
