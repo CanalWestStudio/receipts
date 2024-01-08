cb3c0af1581963a1742d005501a85e9bccd112fb
matt <matt@matts-MacBook-Pro.local>
2024-01-07 22:29:45 -0500

    change line item col 0 to max w 240

--- lib/receipts/base.rb
+++ lib/receipts/base.rb
@@ -111,7 +111,7 @@ module Receipts
         cells.padding = 6
         cells.borders = []
 
-        column(0).width = 200
+        column(0).max_width = 240
         column(-1).style align: :right
 
         row(0..borders).borders = [:bottom]
