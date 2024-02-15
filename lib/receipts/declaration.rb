7ef2aecac4f87ca6a61aefa29144e521fea6dc26
Matt Rundo <matt3r@gmail.com>
2024-02-14 23:38:39 -0500

    vertical_line test

--- lib/receipts/declaration.rb
+++ lib/receipts/declaration.rb
@@ -15,17 +15,16 @@ module Receipts
       render_shipping_details attributes.fetch(:recipients)
       render_line_items attributes.fetch(:line_items)
       render_sub_line_items(sub_line_items) if sub_line_items.present?
-      render_footer attributes.fetch(:footer)
 
-      stroke_color "000000"
-      
-      stroke do
-        rectangle [bounds.left, bounds.top - 36], bounds.width, bounds.top - 36
-      end
+      render_footer attributes.fetch(:footer)
     end
+
+
 
     def render_footer(message, margin_top: 32)
       margin_top
+
+      vertical_line 100, 300, :at => 25
 
       render_footer_stroke if message.present?
       indent(6, 6) do
